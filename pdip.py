91d63184178b9f4cb8672e0ad53f702d4eb568b5
mbbrodie <mikebbrodie@gmail.com>
2020-01-21 21:23:40 +0000

    pdip update

--- pdip.py
+++ pdip.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torch.autograd import Variable
 
 class PDIP(nn.Module):
-    def __init__(self, model=None, size=(1,100,1,1),nlayers=2,device="cpu"):
+    def __init__(self, model=None, arch='prelu', size=(1,100,1,1),nlayers=2,device="cpu"):
         super(PDIP, self).__init__()
         if model is None:
             return
@@ -16,8 +16,9 @@ class PDIP(nn.Module):
             #nn.PReLU(),
             net = nn.Sequential()
             for n in range(nlayers):
-                net.add_module('conv'+str(n), nn.Conv2d(nch, nch, 1, stride=1, bias=False) )
-                net.add_module('prelu'+str(n), nn.PReLU() )
+                net.add_module(make_pdip_block(nch,arch)
+                #net.add_module('conv'+str(n), nn.Conv2d(nch, nch, 1, stride=1, bias=False) )
+                #net.add_module('prelu'+str(n), nn.PReLU() )
             #net = nn.Sequential(nn.Conv2d(nch, nch, 3, 1, 1, bias=False),nn.BatchNorm2d(nch),nn.ReLU()) 
             #net = nn.Sequential(nn.Conv2d(nch, nch, 3, 1, 1, bias=False),nn.BatchNorm2d(nch),nn.ReLU(),nn.Conv2d(nch, nch, 3, 1, 1, bias=False),nn.BatchNorm2d(nch),nn.ReLU()) 
             self.noise.append(net)
@@ -30,6 +31,113 @@ class PDIP(nn.Module):
             x = self.noise[i](x) + x
         x = self.model[-1](x)
         return x
+
+def make_pdip_block(insize,arch,last=False):
+    if arch == 'a' :#a\item BPF + x
+        return nn.Sequential(
+                nn.BatchNorm2d(insize)
+                nn.PReLU(),
+                nn.Conv2d(insize, insize, 1, stride=1, bias=False)
+                )
+    elif arch =='b': #b\item BPF-BF + x
+        return nn.Sequential(
+                nn.BatchNorm2d(insize)
+                nn.PReLU(),
+                nn.Conv2d(insize, insize, 1, stride=1, bias=False)
+
+                nn.BatchNorm2d(insize)
+                nn.Conv2d(insize, insize, 1, stride=1, bias=False)
+                )
+    elif arch =='c': #c\item BPF-BPF$_{bottleneck}$-BF + 
+        return nn.Sequential(
+                nn.BatchNorm2d(insize)
+                nn.PReLU(),
+                nn.Conv2d(insize, insize, 1, stride=1, bias=False)
+
+                nn.BatchNorm2d(insize)
+                nn.PReLU(),
+                nn.Linear(insize,insize/2),
+
+                nn.BatchNorm2d(insize/2)
+                nn.Conv2d(insize/2, insize, 1, stride=1, bias=False)
+                )
+    elif arch =='d': #d\item FBP + x
+        return nn.Sequential(
+                nn.Conv2d(insize, insize, 1, stride=1, bias=False)
+                nn.BatchNorm2d(insize)
+                nn.PReLU(),
+                )
+    elif arch =='e': #e\item FBP-FB + x
+        return nn.Sequential(
+                nn.Conv2d(insize, insize, 1, stride=1, bias=False)
+                nn.BatchNorm2d(insize)
+                nn.PReLU(),
+
+                nn.Conv2d(insize, insize, 1, stride=1, bias=False)
+                nn.BatchNorm2d(insize)
+                )
+    elif arch =='f': #f\item FBP-F_${bottleneck}$BP-FB + 
+        return nn.Sequential(
+                nn.Conv2d(insize, insize, 1, stride=1, bias=False)
+                nn.BatchNorm2d(insize)
+                nn.PReLU(),
+
+                nn.Conv2d(insize, insize/2, 1, stride=1, bias=False)
+                nn.BatchNorm2d(insize/2)
+                nn.PReLU(),
+
+                nn.Conv2d(insize/2, insize, 1, stride=1, bias=False)
+                nn.BatchNorm2d(insize)
+                )
+    elif arch == 'linear'  or last:
+        return nn.Sequential(
+                nn.Linear(insize,insize),
+                )
+    elif arch == 'relu':
+        return nn.Sequential(
+                nn.Linear(insize,insize),
+                nn.ReLU(),
+                )
+    elif arch == 'elu':
+        return nn.Sequential(
+                nn.Linear(insize,insize),
+                nn.ELU(),
+                )
+    elif arch == 'prelu':
+        return nn.Sequential(
+                nn.Conv2d(insize, insize, 1, stride=1, bias=False)
+                nn.PReLU(),
+                )
+    elif arch == 'sigmoid':
+        return nn.Sequential(
+                nn.Linear(insize,insize),
+                nn.Sigmoid(),
+                )
+    elif arch == 'tanh':
+        return nn.Sequential(
+                nn.Linear(insize,insize),
+                nn.Tanh(),
+                )
+    elif arch == 'batchnorm':
+        return nn.Sequential(
+                nn.Linear(insize,insize),
+                nn.BatchNorm1d(insize),
+                )
+    elif arch == 'groupnorm':
+        return nn.Sequential(
+                nn.Linear(insize,insize),
+                nn.GroupNorm(6,1),
+                )
+    elif arch == 'instancenorm':
+        return nn.Sequential(
+                nn.Linear(insize,insize),
+                nn.InstanceNorm1d(insize),
+                )
+    elif arch == 'layernorm':
+        return nn.Sequential(
+                nn.Linear(insize,insize),
+                nn.LayerNorm(insize),
+                )
 
 """
 if __name__ == '__main__':
