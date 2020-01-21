cec14719f650d0ffd4177273b9cd3b11109c626f
mbbrodie <mikebbrodie@gmail.com>
2020-01-21 12:39:34 -0700

    updated exp script

--- run_stylegan_ttt_experiments.py
+++ run_stylegan_ttt_experiments.py
@@ -1,19 +1,83 @@
 ### this will be copied to colab
 ### everything else imported
 from StyleGAN2TTTExperiment import StyleGAN2TTTExperiment as e
+
+
+def run(args):
+    e.set_args(args)
+    ## SETUP
+    #if args.TTT:
+    if args.method == 'TNet+TTT' or args.method == 'TTTz':
+        e.setup_prenetwork_ttt()
+    if args.method == 'TNet+TTT' or args.method == 'TTTw':
+        e.setup_prenetwork_w_ttt()
+    if args.method == 'TNet+TTT' or args.method == 'TNet':
+        e.setup_intranetwork_ttt()
+
+    ## TRAIN
+    # may need to set train_sample_size
+    if args.method == 'TNet+TTT':
+        e.train_prenetwork_and_intranetwork_ttt()
+    elif args.method == 'TNet':
+        e.train_intranetwork_ttt()
+    elif args.method == 'TTTz':
+        e.train_prenetwork_ttt()
+    elif args.method == 'TTTw':
+        e.train_prenetwork_w_ttt()
+        
+    for i in range(args.n_eval_samples):
+        ##comparison methods
+        #if args.method in comparison_methods: #= ['normal','coachz','coachw','ttz','ttw']
+        if args.method == 'normal':
+            e.gen_from_w() 
+        if args.method == 'coachz':
+            e.sample_n_stylegan_images_with_coachgan()
+        if args.method == 'coachw':
+            e.sample_n_stylegan_images_with_w_coachgan()
+        if args.method == 'ttz':
+            e.sample_n_stylegan_images_with_z_tt()
+        if args.method == 'ttw':
+            e.sample_n_stylegan_images_with_w_ttl()
+
+        ## TTT and TNET  
+        if args.method == 'TTTz':
+            e.sample_n_stylegan_images_with_prenetwork_ttt()
+        if args.method == 'TTTw':
+            e.sample_n_stylegan_images_with_post_w_prenetwork_ttt()
+        if args.method == 'TNet':
+            e.sample_n_stylegan_images_with_intranetwork_ttt()
+        if args.method == 'TNet+TTT':
+            e.sample_n_stylegan_images_with_pre_and_intranetwork_ttt()
+
+        e.save_results()
+    #metrics.calc_metrics()
+
+#parser = argparse.ArgumentParser()
+#parser.add_argument('--TT', action='store_true', help='use TT for z')
+#parser.add_argument('--TTl', action='store_true', help='use TT-lerp for w')
+#parser.add_argument('--TTT', action='store_true', help='use TTT for z')
+#parser.add_argument('--w_TTT', action='store_true', help='use TTT for w')
+#parser.add_argument('--coach_z', action='store_true', help='use CoachGAN for z')
+#parser.add_argument('--coach_w', action='store_true', help='use CoachGAN for w')
 import os.path.join as join
 import easydict
 args = easydict.EasyDict()
 args.n_eval_samples = 10
-args.path = '' #gdrive probably
+args.path = '/content' #gdrive probably
 import metrics
 
 #comparison methods
+datasets = ['ffhq','cat','horse','churche','car']
+args.dataset = ['ffhq']
+args.path ='/content'
+
 comparison_methods = ['normal','coachz','coachw','ttz','ttw']
 for method in comparison_methods:
+    print('method:',method)
     args.method = method
     args.savedir = join( args.path, args.base_exp_name,  method)
     run(args)
+    exit() #TEMPORARY
 
 ## SAMPLE
 experiments= [
@@ -45,61 +109,3 @@ for nl in nlayer
             args.method = method
             args.savedir = join( args.base_exp_name,  method)
             run(args)
-
-def run(args):
-    e.set_args(args)
-    ## SETUP
-    if args.TTT:
-        setup_prenetwork_ttt()
-    if args.W_TTT:
-        setup_prenetwork_w_ttt()
-    if args.TNET:
-        setup_intranetwork_ttt()
-
-    ## TRAIN
-    # may need to set train_sample_size
-    if args.TNET and args.TTT and args.W_TTT: 
-        train_prenetwork_and_intranetwork_ttt()
-    elif args.TNET:
-        train_intranetwork_ttt()
-    elif args.TTT:
-        train_prenetwork_ttt()
-    elif args.W_TTT:
-        train_prenetwork_w_ttt()
-        
-    for i in range(args.n_eval_samples):
-        ##comparison methods
-        #if args.method in comparison_methods: #= ['normal','coachz','coachw','ttz','ttw']
-        if args.method == 'normal':
-            gen_from_w() # normal
-        if args.method == 'coachz':
-            sample_n_stylegan_images_with_coachgan()
-        if args.method == 'coachw':
-            sample_n_stylegan_images_with_w_coachgan()
-        if args.method == 'ttz':
-            sample_n_stylegan_images_with_z_tt()
-        if args.method == 'ttw':
-            sample_n_stylegan_images_with_w_ttl()
-
-        ## TTT and TNET  
-        if args.method == 'TTTz':
-            sample_n_stylegan_images_with_prenetwork_ttt()
-        if args.method == 'TTTw':
-            sample_n_stylegan_images_with_post_w_prenetwork_ttt()
-        if args.method == 'TNet':
-            sample_n_stylegan_images_with_intranetwork_ttt()
-        if args.method == 'TNet+TTT':
-            sample_n_stylegan_images_with_pre_and_intranetwork_ttt()
-
-        save_results()
-    metrics.calc_metrics()
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--TT', action='store_true', help='use TT for z')
-    parser.add_argument('--TTl', action='store_true', help='use TT-lerp for w')
-    parser.add_argument('--TTT', action='store_true', help='use TTT for z')
-    parser.add_argument('--w_TTT', action='store_true', help='use TTT for w')
-    parser.add_argument('--coach_z', action='store_true', help='use CoachGAN for z')
-    parser.add_argument('--coach_w', action='store_true', help='use CoachGAN for w')
-
